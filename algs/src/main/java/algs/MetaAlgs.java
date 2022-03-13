59e390c9b4ffd07209f8a483ce64ca358fe6778f
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2022-03-13 15:35:14 +0100

    Add DataReaderI for data parsing

--- algs/src/main/java/algs/MetaAlgs.java
+++ algs/src/main/java/algs/MetaAlgs.java
@@ -1,4 +1,6 @@
-package algs;
+package main.java.algs;
+
+import java.io.FileNotFoundException;
 
 public class MetaAlgs
 {
@@ -6,7 +8,18 @@ public class MetaAlgs
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
-        
+        System.out.println(System.getProperty("user.dir"));
+        try
+        {
+            ProblemInstance pi = DataReaderI.readFileForGraphMatrix(args[0]);
+            System.out.println("AAA");
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
     }
+
     
 }

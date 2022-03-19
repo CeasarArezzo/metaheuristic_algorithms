49a7acccf75d2fd5b435d8708f39fc40a846ef65
AdamChojnackiLaptop <adam.chojnacki.syl@gmail.com>
2022-03-19 22:50:58 +0100

    Implement Opt2 metaherustic class

--- src/main/java/solver/Opt2Solver.java
+++ src/main/java/solver/Opt2Solver.java
@@ -3,9 +3,7 @@ package solver;
 import algs.ProblemInstance;
 import solution.ProblemSolution;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class Opt2Solver implements ProblemSolver
 {
@@ -15,8 +13,65 @@ public class Opt2Solver implements ProblemSolver
     {
         int dimension = problemInstance.getDimension();
         ArrayList<Integer> startingSolution = generateRandomPath(dimension);
-        return null;
+        ArrayList<Integer> bestPath = new ArrayList<>();
+        int bestValue = -1;
+        NeighbourHolder bestNeighbour;
+
+        while(true)
+        {
+            bestNeighbour = getBestNeighbour(startingSolution, problemInstance);
+            if(bestNeighbour.objectiveValue < bestValue)
+            {
+                bestValue = bestNeighbour.objectiveValue;
+                bestPath = bestNeighbour.path;
+                continue;
+            }
+            break;
+        }
+
+        return new ProblemSolution(bestPath.get(0), bestNeighbour.objectiveValue, bestPath, problemInstance);
     }
 
+    private NeighbourHolder getBestNeighbour(ArrayList<Integer> startingSolution, ProblemInstance problemInstance)
+    {
+        ArrayList<ArrayList<Integer>> neighbours = generateAllNeighbours(startingSolution, problemInstance);
+        int dimension = problemInstance.getDimension();   
+        ArrayList<Integer> bestNeighbour = neighbours.get(0);
+        int bestNeighbourValue = ProblemSolution.getObjectiveValue(bestNeighbour.get(0), bestNeighbour, problemInstance);
+        
+        for(int i = 1; i < dimension; i++)
+        {
+            ArrayList<Integer> currNeighbour = neighbours.get(i);
+            int currValue = ProblemSolution.getObjectiveValue(currNeighbour.get(0), currNeighbour, problemInstance);
 
+            if( currValue > bestNeighbourValue)
+            {
+                bestNeighbourValue = currValue;
+                bestNeighbour = currNeighbour;
+            }
+        }
+        
+        return new NeighbourHolder(bestNeighbour, bestNeighbourValue);
+    }
+
+    private ArrayList<ArrayList<Integer>> generateAllNeighbours(ArrayList<Integer> startingSolution, ProblemInstance problemInstance)
+    {
+        ArrayList<ArrayList<Integer>> neighbours = new ArrayList<>();
+        int dimension = problemInstance.getDimension();
+        for(int i = 0; i < dimension; i++)
+        {
+            for(int j = i + 1; j < dimension; j++)
+            {
+                ArrayList<Integer> newNeighbour = new ArrayList<>(startingSolution);
+                Collections.swap(newNeighbour, i, j);
+                neighbours.add(newNeighbour);
+            }
+        }
+
+        return neighbours;
+    }
+
+    private record NeighbourHolder(ArrayList<Integer> path, int objectiveValue)
+    {
+    }
 }

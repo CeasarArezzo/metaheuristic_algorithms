06dc188cb4852b991f41398a89a6927151e3e9bf
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2022-03-30 20:30:54 +0200

    Fix generating random path

--- src/main/java/solver/ProblemSolver.java
+++ src/main/java/solver/ProblemSolver.java
@@ -4,6 +4,7 @@ import algs.ProblemInstance;
 import solution.ProblemSolution;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -13,10 +14,10 @@ public interface ProblemSolver
 
     default public ArrayList<Integer> generateRandomPath(int dimension)
     {
-        List<Integer> range = IntStream.rangeClosed(0, dimension - 1)
-                .boxed().toList();
+        ArrayList<Integer> range = new ArrayList<>(IntStream.rangeClosed(0, dimension - 1)
+                .boxed().toList());
 
-        java.util.Collections.shuffle(range);
+        Collections.shuffle(range);
 
         return new ArrayList<>(range);
     }

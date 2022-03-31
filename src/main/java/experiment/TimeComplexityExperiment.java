71c03a2930bce9511598834d99b0c5153fcc5c80
CeasarArezzo <ceasararezzo@gmail.com>
2022-03-31 12:46:42 +0200

    fix logarithmic graphs

--- src/main/java/experiment/TimeComplexityExperiment.java
+++ src/main/java/experiment/TimeComplexityExperiment.java
@@ -6,6 +6,8 @@ import java.nio.charset.StandardCharsets;
 
 import algs.ProblemInstance;
 import generator.BasicATSPProblemGenerator;
+import generator.BasicTSPProblemGenerator;
+import generator.ProblemGenerator;
 import solution.ProblemSolution;
 import solver.ClosestNeighSolver;
 import solver.EnhancedNeighSolver;
@@ -16,7 +18,7 @@ import solver.ProblemSolver;
 public class TimeComplexityExperiment
 {
     static final int ITERATIONS = 500;
-    static final int REPEATS = 15;
+    static final int REPEATS = 10;
     
     public static void generateData()
     {
@@ -29,15 +31,15 @@ public class TimeComplexityExperiment
             writer.write("size KRandom ClosestNeigh EnchancedNeigh Opt2\n");
             ProblemSolver[] solvers = {new KRandomSolver(100), new ClosestNeighSolver(0), new EnhancedNeighSolver(), new Opt2Solver()};
             
-            for (int size = 10; size <= ITERATIONS; size += 25)
+            for (int size = 10; size <= ITERATIONS; size += 26)
             {
                 System.out.println(size);
-                BasicATSPProblemGenerator generator = new BasicATSPProblemGenerator();
+                ProblemGenerator generator = new BasicTSPProblemGenerator();
                 writer.write(size + " ");
                 int[] sums = new int[solvers.length];
                 for (int i = 0; i < REPEATS; i++)
                 {
-                    ProblemInstance pI = generator.generateATSPProblemInstance(size);
+                    ProblemInstance pI = generator.generateProblemInstance(size);
                     for (int k = 0; k < solvers.length; k++)
                     {
                         long start = System.nanoTime();

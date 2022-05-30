16f9d2eabe547a1c07c8892b1343075bab27dceb
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2022-05-30 14:45:46 +0200

    Cleanup

--- src/main/java/solver/bees/Bee.java
+++ src/main/java/solver/bees/Bee.java
@@ -2,7 +2,6 @@ package solver.bees;
 
 import algs.ProblemInstance;
 import solution.ProblemSolution;
-import solver.ProblemSolver;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,21 +11,23 @@ import java.util.stream.IntStream;
 
 public class Bee implements Runnable
 {
-    private final Random randomizer;
-    private ArrayList<Integer> myPlace;
-
+    //External utils//
     private final ProblemInstance pInstance;
+    private final Random randomizer;
 
-    private int failCounter = 0;
-
-    private final int failThreshold;
-
+    //Bee state//
+    private ArrayList<Integer> myPlace;
     private BeePhase phase;
-
     private double fitOfMyPlace;
 
+    //Fail control utils//
+    private int failCounter = 0;
+    private final int failThreshold;
+
+
+    //onlooker fields//
     private ArrayList<ArrayList<Integer>> allFoodSources;
-    private double[] probs;
+    private double[] foodSourceProbDist;
 
 
     public Bee(ArrayList<Integer> yourPlace, ProblemInstance pInstance, int failThreshold)
@@ -38,6 +39,81 @@ public class Bee implements Runnable
         this.failThreshold = failThreshold;
 
     }
+
+    //main thread function//
+    @Override
+    public void run()
+    {
+        switch (phase)
+        {
+            case EmployBee -> BeeEmployBee();
+            case Onlooker -> BeeOnlooker();
+            case Scout -> BeeScout();
+        }
+    }
+
+    //Bee state jobs//
+    private void BeeEmployBee()
+    {
+        ArrayList<Integer> neighPlace = findNeighbouringFoodSource();
+
+        if(fitness(myPlace) > fitness(neighPlace))
+        {
+            failCounter++;
+        }
+        else
+        {
+            myPlace = new ArrayList<>(neighPlace);
+            fitOfMyPlace = fitness(myPlace);
+            failCounter = 0;
+        }
+
+    }
+
+    private void BeeOnlooker()
+    {
+        myPlace = new ArrayList<>(allFoodSources.get(onlookerChooseFsIndex()));
+        fitOfMyPlace = fitness(myPlace);
+        BeeEmployBee();
+    }
+
+    private void BeeScout()
+    {
+        if(failCounter >= failThreshold)
+        {
+            myPlace = new ArrayList<>(IntStream.rangeClosed(0, pInstance.getDimension() - 1)
+                    .boxed().toList());
+
+            Collections.shuffle(myPlace);
+            fitOfMyPlace = fitness(myPlace);
+        }
+        else
+        {
+            BeeEmployBee();
+        }
+    }
+
+
+    //Bee state swappers//
+    public void swapToEmployBee()
+    {
+        phase = BeePhase.EmployBee;
+    }
+
+    public void swapToOnlooker(ArrayList<ArrayList<Integer>> allFoodSources, double[] probs)
+    {
+        this.allFoodSources = allFoodSources;
+        this.foodSourceProbDist = probs;
+        phase = BeePhase.Onlooker;
+    }
+
+    public void swapToScout()
+    {
+        phase = BeePhase.Scout;
+    }
+
+
+    //Utils SECTION//
     private ArrayList<Integer> findNeighbouringFoodSource()
     {
 
@@ -79,78 +155,6 @@ public class Bee implements Runnable
 
     }
 
-    private void BeeEmployBee()
-    {
-        ArrayList<Integer> neighPlace = findNeighbouringFoodSource();
-
-        if(fitness(myPlace) > fitness(neighPlace))
-        {
-            failCounter++;
-        }
-        else
-        {
-            myPlace = new ArrayList<>(neighPlace);
-            fitOfMyPlace = fitness(myPlace);
-            failCounter = 0;
-        }
-
-    }
-
-    private void BeeOnlooker()
-    {
-        myPlace = new ArrayList<>(allFoodSources.get(onlookerChooseFsIndex()));
-        fitOfMyPlace = fitness(myPlace);
-        BeeEmployBee();
-    }
-
-    private void BeeScout()
-    {
-        if(failCounter >= failThreshold)
-        {
-            myPlace = new ArrayList<>(IntStream.rangeClosed(0, pInstance.getDimension() - 1)
-                    .boxed().toList());
-
-            Collections.shuffle(myPlace);
-            fitOfMyPlace = fitness(myPlace);
-        }
-        else
-        {
-            BeeEmployBee();
-        }
-    }
-
-    @Override
-    public void run()
-    {
-        switch (phase)
-        {
-            case EmployBee -> BeeEmployBee();
-            case Onlooker -> BeeOnlooker();
-            case Scout -> BeeScout();
-        }
-    }
-
-
-
-
-    public void swapToEmployBee()
-    {
-        phase = BeePhase.EmployBee;
-    }
-
-    public void swapToOnlooker(ArrayList<ArrayList<Integer>> allFoodSources, double[] probs)
-    {
-        this.allFoodSources = allFoodSources;
-        this.probs = probs;
-        phase = BeePhase.Onlooker;
-    }
-
-    public void swapToScout()
-    {
-        phase = BeePhase.Scout;
-    }
-
-
     public ArrayList<Integer> getMyPlace()
     {
         return myPlace;
@@ -163,20 +167,20 @@ public class Bee implements Runnable
     }
 
 
-    public int onlookerChooseFsIndex()
+    private int onlookerChooseFsIndex()
     {
 
-        double random = ThreadLocalRandom.current().nextDouble(0.0, probs[probs.length - 1]);
+        double random = ThreadLocalRandom.current().nextDouble(0.0, foodSourceProbDist[foodSourceProbDist.length - 1]);
 
-        for(int i = 0; i < probs.length; i++)
+        for(int i = 0; i < foodSourceProbDist.length; i++)
         {
-            if(random < probs[i])
+            if(random < foodSourceProbDist[i])
             {
                 return i;
             }
         }
 
-        return probs.length- 1;
+        return foodSourceProbDist.length- 1;
     }
 
 }
